90d5835c5ce4be8945c46dca913dc5e06e9d844f
Eduardo Urias <windkaiser@gmail.com>
2015-09-25 17:51:02 -0700

    added: example setting an env var with

--- proc/environ.c
+++ proc/environ.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
+#include <assert.h>
 
-// environ can be accessed this way as well
-// extern char ** environ;
-
-int main(int argc, char ** argv, char ** environ) {
+ extern char ** environ;
+                                
+int main(int argc, char ** argv/*, char ** environ */) {
     char **ep;
 
     for (ep = environ; *ep != NULL; ep++) {
         puts(*ep);
     }
+
+    // inspecting an env var
+    char *current_shell = getenv("SHELL");
+    if (current_shell != NULL)
+        printf("The current SHELL is %s\n", current_shell);
+
+    // setting an env var
+    setenv("foo", "bar", 1);
+    assert(strcmp(getenv("foo"), "bar") == 0);
 
     exit(EXIT_SUCCESS);
 }
